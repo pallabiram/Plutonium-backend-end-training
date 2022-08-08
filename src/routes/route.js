5d08535b7e34d5b071b3627086fdf43495eab3d9
pallabiram <kumaripallavi5539@gmail.com>
2022-08-08 23:43:20 +0530

    8 Aug assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,78 @@
-const { response } = require('express');
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-const linkLogger=require('../logger/logger')
 
-const linkhelper=require('../util/helper')
-
-const linkformattor=require('../validator/formattor')
-router.get('/test-me',function(req, res){
-   linkformattor.gettrim()
-   linkformattor.getlowercase()
-   linkformattor.getuppercase()
-
-    res.send('Module 3 successfully completed')
+router.get('/movies', function (req, res) {
+    let movieList = ["Dhamaal", "All the best", "Chup chup ke", "Golmaal", "Hera Pheri"]
+    console.log(movieList)
+    res.send("Successfully Print the All Movies Name on Console")
 })
 
-router.get('/test-me',function(req, res){
-    linkhelper.getDate() 
-    linkhelper.getMonth()
-    linkhelper.printBatchInfo()
-    res.send('Module 2 successfully completed')
+router.get('/movies/:indexNumber', function (req, res) {
+    let movieList = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let movieIndex = req.params.indexNumber
+    if (movieIndex < movieList.length) {
+        console.log([movieList[movieIndex]])
+        res.send("Successfully Print the Movie Name on Console")
+    }
+    else {
+        console.log("Please use a valid Index Number")
+        res.send("Please use a valid Index Number")
+    }
 })
 
-router.get('/test-me',function(req, res){
-    linkLogger.printWelcome()
-    res.send('Module 1 successfully completed')
+router.get('/films', function (req, res) {
+    let filmObject = [{
+        "id": 1,
+        "name": "The Shining"
+    },
+    {
+        "id": 2,
+        "name": "Incendies"
+    },
+    {
+        "id": 3,
+        "name": "Rang de Basanti"
+    },
+    {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+    console.log(filmObject)
+    res.send("Successfully Print the All Films Name on Console")
 })
+
+
+router.get('/films/:filmId', function(req, res){
+    let filmObject = [{
+        "id": 1,
+        "name": "The Shining"
+    },
+    {
+        "id": 2,
+        "name": "Incendies"
+    },
+    {
+        "id": 3,
+        "name": "Rang de Basanti"
+    },
+    {
+        "id": 4,
+        "name": "Finding Nemo"
+    }] 
+
+    let filmId = req.params.filmId
+    if(filmId<filmObject.length)
+    {
+        console.log(filmObject[filmId])
+        res.send("Successfully Print the Films Name on Console")
+    }
+    else{
+        console.log("No movie exists with this id")
+        res.send("No movie exists with this id")
+    }
+})
+
 
 
 
@@ -36,13 +83,29 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
+router.get('/students', function (req, res) {
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
 })
 
-router.get('/give-me-students-data',function(req, res){
+router.get('/student-details/:name', function (req, res) {
+    /*
+    params is an attribute inside request that contains 
+    dynamic values.
+    This value comes from the request url in the form of an 
+    object where key is the variable defined in code 
+    and value is what is sent in the request
+    */
 
+    let requestParams = req.params
+
+    // JSON strigify function helps to print an entire object
+    // We can use any ways to print an object in Javascript, JSON stringify is one of them
+    console.log("This is the request " + JSON.stringify(requestParams))
+    let studentName = requestParams.name
+    console.log('Name of the student is ', studentName)
+
+    res.send('Dummy response')
 })
+
 module.exports = router;
-// adding this comment for no reason
