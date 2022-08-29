523eda73d42f5c823c731909370a71a2022e0dac
pallabiram <kumaripallavi5539@gmail.com>
2022-08-29 22:08:07 +0530

    authorisation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,5 @@
 
+const jwt = require("jsonwebtoken")
 const auth= function(req,res,next){
 let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
@@ -8,9 +9,17 @@ let token = req.headers["x-Auth-token"];
   // console.log(token);
 
   let decodedToken = jwt.verify(token, "it is very secret");
+  // console.log(decodedToken)
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
+
+    let paramsUserId = req.params.userId
+    let userLoggedIn = decodedToken.userId
+
+    if (paramsUserId != userLoggedIn) {
+        return res.send({ msg: "This Token is not allowed for this user ID" })
+    }
     next()
 }
 
-module.exports.auth=au
+module.exports.auth=auth
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 const usermiddleware=require("../middleware/auth")
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -13,7 +14,7 @@ router.post("/login", userController.loginUser)
 //The userId is sent by front end
 router.get("/users/:userId", usermiddleware.auth, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId", usermiddleware.auth, userController.updateUser)
 
 router.delete("/users/:userId",usermiddleware.auth, userController.deleteUser)
 
