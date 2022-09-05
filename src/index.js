46a76651cad70f5cb6c35ad2e9530b31cfeec3ae
pallabiram <kumaripallavi5539@gmail.com>
2022-09-05 16:34:54 +0530

    Author-Schema

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const bodyParser = require('body-parser')
+const express=require('express')
+const mongoose=require('mongoose')
+
+const app=express()
+
+app.listen(3000,function(){console.log("code is running sucsessfully")})
---
+++ src/models/authorModel.js
@@ -0,0 +1,14 @@
+const mongoose= require('mongoose')
+
+const authorSchema=new mongoose.Schema({
+     fname: {type:String, require:true},
+     lname: {type:String, require:true},
+     title: {type:String, require:true,enum:["Mr", "Mrs", "Miss"]},
+       
+     email: {type:String, require:true},
+      password: {type:String, require:true} 
+},
+{timestamps:true})
+
+module.exports=mongoose.model('author', authorSchema)
+
