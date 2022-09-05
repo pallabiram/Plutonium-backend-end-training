7f1da49493e713079072fbb07775a407dbb5ce77
kamrebaba <alamkamre58@gmail.com>
2022-09-05 18:04:11 +0530

    creating 1st post api

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const authorController= require("../controller/authorController")
+const bookController = require("../controller/blogController")
 router.get('/test-me',function(req,res){
     res.send({msg : "test done "})
 })
 
+router.post("/authors",authorController.createAuthor)
 
 
 
